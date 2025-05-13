7e437f8efca8442afd77dd22a44f40dc75757168
Juniper Beatitudes <weirdgirl98@proton.me>
2025-05-13 08:15:38 -0500

    Added some initial code for bootloader

---
+++ kernel/include/common.h
@@ -0,0 +1,10 @@
+#ifndef BEA_KERN_COMMON_H
+#define BEA_KERN_COMMON_H
+
+typedef int bool_t;
+#define TRUE 0x1
+#define FALSE 0X0
+
+#define NULL 0x0
+
+#endif
---
+++ kernel/include/entry.h
@@ -0,0 +1,19 @@
+#ifndef BEA_KERN_ENTRY_H
+#define BEA_KERN_ENTRY_H
+
+#define STACK_TOP (void *)(0x20030000)
+
+#include "common.h"
+
+void bea_kern_onreset (void);
+
+typedef void (*ivt_entry_t) (void);
+
+__attribute__ ((section (".ivt"))) const static ivt_entry_t IVT[] = {
+        (ivt_entry_t)STACK_TOP,
+        bea_kern_onreset,
+        NULL,
+        NULL,
+};
+
+#endif
