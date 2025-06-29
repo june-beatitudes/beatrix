002bb777ce7a57685b5757e9dfc526b1c18babe4
Juniper Beatitudes <weirdgirl98@proton.me>
2025-06-28 19:31:42 -0700

    Added some more stuff to common

--- kernel/include/common.h
+++ kernel/include/common.h
@@ -4,5 +4,14 @@
 #define NULL 0x0
 
 typedef unsigned long size_t;
+typedef unsigned long uint64_t;
+typedef unsigned int uint32_t;
+typedef unsigned short uint16_t;
+typedef unsigned char uint8_t;
+typedef enum
+{
+  true,
+  false,
+} bool;
 
 #endif
