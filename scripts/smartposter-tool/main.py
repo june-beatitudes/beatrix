89cf91ee926987745e3b586a268e613a87c167c3
Juniper Beatitudes <weirdgirl98@proton.me>
2025-07-26 12:27:31 -0700

    Started work on NFC smartposter bin generating tool

---
+++ scripts/smartposter-tool/main.py
@@ -0,0 +1,11 @@
+import ndef
+
+
+def main():
+    # A smart-poster NDEF message is a little meta, so let's start small
+
+    print("Hello from smartposter-tool!")
+
+
+if __name__ == "__main__":
+    main()
